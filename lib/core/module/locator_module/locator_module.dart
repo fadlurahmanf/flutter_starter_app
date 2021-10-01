bdc2a0d7e67dd1131abb75f461de0a235da23dac
tffaj <taufik.fajari@akarinti.tech>
2021-10-01 17:28:54 +0700

    add http client for injection

--- lib/core/module/locator_module/locator_module.dart
+++ lib/core/module/locator_module/locator_module.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_starter_app/core/http_client/http_dio_client.dart';
 import 'package:flutter_starter_app/ui/example1/datasources/post_datasources.dart';
 import 'package:flutter_starter_app/utils/shared_preferences/utils_sharedpreferences.dart';
 import 'package:get_it/get_it.dart';
@@ -7,6 +8,7 @@ GetIt locator = GetIt.instance;
 class LocatorModule{
   static void init(){
     locator.registerSingleton(UtilsSharedPreferences());
+    locator.registerSingleton(HttpClientDio(buildConfig: GetIt.I()));
     locator.registerFactory(() => PostDataImpl());
   }
 }
