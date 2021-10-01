bbe1c799c3032d7ee9d359b58e586ed0c36af813
tffaj <taufik.fajari@akarinti.tech>
2021-10-01 16:38:13 +0700

    add module injection

--- lib/core/module/locator_module/locator_module.dart
+++ lib/core/module/locator_module/locator_module.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_starter_app/ui/example1/datasources/post_datasources.dart';
 import 'package:flutter_starter_app/utils/shared_preferences/utils_sharedpreferences.dart';
 import 'package:get_it/get_it.dart';
 
@@ -6,5 +7,6 @@ GetIt locator = GetIt.instance;
 class LocatorModule{
   static void init(){
     locator.registerSingleton(UtilsSharedPreferences());
+    locator.registerFactory(() => PostDataImpl());
   }
 }
