84ad2e0f2dc4dd6781f94252649dbebbf2618891
tffaj <taufik.fajari@akarinti.tech>
2021-10-01 15:36:31 +0700

    update text color in commons_textstyle.dart

--- lib/commons/commons_textstyle.dart
+++ lib/commons/commons_textstyle.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_starter_app/constant/constant_color.dart';
 import 'package:flutter_starter_app/constant/constant_fonts.dart';
 
+
+//TODO ADD FONT STYLE HERE
 class CommonsTextStyle{
-  static TextStyle get body18RegBlack{
+  static TextStyle get body18RegBlackGrey{
     return TextStyle(fontFamily: ConstantFont.metropolis,
         fontWeight: FontWeight.w400,
         fontSize: 18,
-        color: Colors.black
+        color: ConstantColor.blackGrey
     );
   }
 }
