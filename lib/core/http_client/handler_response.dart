af3980e1f05a4cbeccf6a3b17d280ffa86926552
tffaj <taufik.fajari@akarinti.tech>
2021-10-01 16:38:58 +0700

    create init handler response for http_client

---
+++ lib/core/http_client/handler_response.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+Future<T> handleResponse<T>(
+    {required Future<Response<dynamic>> Function() request,
+      required T Function(Response<dynamic>) onSuccess}) async {
+  try {
+    final result = await request();
+    return onSuccess(result);
+  } on DioError catch (e) {
+    var message = e.message;
+    throw message;
+  } catch (e) {
+    var message = e.toString();
+    throw message;
+  }
+}
